bcd4753264ae7a7ed13dc3b9987d9b54c461133e
Freemasoid <freemasoid@icloud.com>
2024-04-07 18:06:23 +0200

    outsourced file reading func to filemanager package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"github.com/Freemasoid/go-practice-price-calculator/conversion"
+	"github.com/Freemasoid/go-practice-price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -15,26 +14,11 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+
+	lines, err := filemanager.ReadLines("prices.txt")
 
 	if err != nil {
-		fmt.Println("could not open a file")
 		fmt.Println(err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("reading file content failed")
-		fmt.Println(err)
-		file.Close()
 		return
 	}
 
@@ -42,12 +26,10 @@ func (job *TaxIncludedPriceJob) LoadData() {
 
 	if err != nil {
 		fmt.Println(err)
-		file.Close()
 		return
 	}
 
 	job.InputPrices = prices
-	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
